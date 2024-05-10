ae27d2819b4152f003a122daf1446fc92a5dbc3c
xFerdx <konradroth74@gmail.com>
2024-05-10 20:59:58 +0200

    c2

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -4,48 +4,176 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class MyPanel extends JPanel{
 
     int[][] points = new int[0][2];
-
-    float[][] lengths = new float[0][0];
-
-    int[] solution = new int[0];
-
-    final int pointSize = 10;
+    ArrayList<Integer> solution = new ArrayList<>();
+    final int pointSize = 6;
 
     MyPanel(){
         this.setPreferredSize(new Dimension(500,500));
         setFocusable(true);
         requestFocus();
 
-        JTextField textField = new JTextField(5);
+        JLabel label = new JLabel();
 
+        JTextField textField = new JTextField(5);
         JButton button = new JButton("create Points");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                redrawButtonClicked(Integer.parseInt(textField.getText()));
+                generateNewPoints(Integer.parseInt(textField.getText()));
             }
         });
+
+
         JButton button2 = new JButton("BF");
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                bruteForce();
+                TSPSolver tsp = new TSPSolver(getLengths());
+                solution = tsp.bruteForce();
+                System.out.println(solution.toString());
+                label.setText(String.valueOf(solution));
+                repaint();
             }
         });
 
+        JButton button3 = new JButton("HK");
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TSPSolver tsp = new TSPSolver(getLengths());
+                solution = tsp.tspHeldKarp();
+                System.out.println(solution.toString());
+                label.setText(String.valueOf(tsp.calcLength(solution)));
+                repaint();
+            }
+        });
+
+        JButton button4 = new JButton("NN");
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TSPSolver tsp = new TSPSolver(getLengths());
+                solution = tsp.nn();
+                System.out.println(solution.toString());
+                label.setText(String.valueOf(tsp.calcLength(solution)));
+                repaint();
+            }
+        });
+
+        JButton button5 = new JButton("NN2");
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TSPSolver tsp = new TSPSolver(getLengths());
+                solution = tsp.nn2();
+                System.out.println(solution.toString());
+                label.setText(String.valueOf(tsp.calcLength(solution)));
+                repaint();
+            }
+        });
+
+        JButton button6 = new JButton("Ant2");
+        button6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TSPSolver tsp = new TSPSolver(getLengths());
+                solution = null;
+                System.out.println(solution.toString());
+                label.setText(String.valueOf(tsp.calcLength(solution)));
+                repaint();
+            }
+        });
+
+        JButton button7 = new JButton("rand");
+        button7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TSPSolver tsp = new TSPSolver(getLengths());
+                solution = tsp.randomRoute();
+                System.out.println(solution.toString());
+                label.setText(String.valueOf(tsp.calcLength(solution)));
+                repaint();
+            }
+        });
+
+        JButton button8 = new JButton("Ant");
+        button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TSPSolver tsp = new TSPSolver(getLengths());
+                solution = tsp.ant();
+                System.out.println(solution.toString());
+                label.setText(String.valueOf(tsp.calcLength(solution)));
+                repaint();
+            }
+        });
+
+        JButton button9 = new JButton("CalcAll");
+        button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 2; i < 20; i++) {
+                    float sum = 0;
+                    for (int j = 0; j < 100; j++) {
+                        generateNewPoints(i);
+                        TSPSolver tsp = new TSPSolver(getLengths());
+                        solution = tsp.tspHeldKarp();
+                        sum += tsp.calcLength(solution);
+                    }
+                    sum /= 100;
+                    System.out.println(i+": "+sum);
+                }
+            }
+        });
+
+
+        JTextField textField2 = new JTextField(5);
+        JButton button10 = new JButton("Opt");
+        button10.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TSPSolver tsp = new TSPSolver(getLengths());
+                float oldLen = tsp.calcLength(solution);
+                float newLen;
+                while(true){
+                    solution = tsp.opt(solution, Integer.parseInt(textField2.getText()));
+                    newLen = tsp.calcLength(solution);
+                    if(oldLen==newLen)break;
+                    oldLen=newLen;
+                }
+
+                System.out.println(solution.toString());
+
+
+                label.setText(String.valueOf(tsp.calcLength(solution)));
+                repaint();
+            }
+        });
+
+        add(label);
         add(textField);
         add(button);
         add(button2);
+        add(button3);
+        add(button4);
+        add(button5);
+        add(button6);
+        add(button7);
+        add(button8);
+        add(button9);
+        add(textField2);
+        add(button10);
 
 
     }
 
-    public void redrawButtonClicked(int number){
-        solution = new int[0];
+    public void generateNewPoints(int number){
+        solution = new ArrayList<>();
         points = new int[number][2];
         for (int i = 0; i < number; i++) {
             points[i][0] = (int) (Math.random()*300)+100;
@@ -55,13 +183,14 @@ public class MyPanel extends JPanel{
         repaint();
     }
 
-    public void getLengths(){
-        lengths = new float[points.length][points.length];
+    public float[][] getLengths(){
+        float[][] lengths = new float[points.length][points.length];
         for (int i = 0; i < points.length; i++) {
             for (int j = 0; j < points.length; j++) {
                 lengths[i][j] = (float) Math.sqrt(Math.pow(points[i][0]-points[j][0],2)+Math.pow(points[i][1]-points[j][1],2));
             }
         }
+        return lengths;
     }
 
     public void paint(Graphics g){
@@ -72,49 +201,16 @@ public class MyPanel extends JPanel{
             int[] point = points[i];
             g2d.setColor(i==0?Color.GREEN:Color.BLUE);
             g2d.fillOval(point[0], point[1], pointSize, pointSize);
+            g2d.drawString(String.valueOf(i),point[0],point[1]);
         }
 
-        for (int i = 0; i < solution.length-1; i++) {
+        for (int i = 0; i < solution.size()-1; i++) {
             g2d.setColor(Color.BLACK);
-            g2d.drawLine(points[solution[i]][0]+pointSize/2,points[solution[i]][1]+pointSize/2,points[solution[i+1]][0]+pointSize/2,points[solution[i+1]][1]+pointSize/2);
+            g2d.drawLine(points[solution.get(i)][0]+pointSize/2,points[solution.get(i)][1]+pointSize/2,points[solution.get(i+1)][0]+pointSize/2,points[solution.get(i+1)][1]+pointSize/2);
         }
     }
 
-    public void bruteForce(){
-        ArrayList<Integer> visited = new ArrayList<>();
-        visited.add(0);
-        float[] minVal = {Integer.MAX_VALUE};
-        bfRec(visited, minVal);
-        repaint();
-        System.out.println("minVal: "+minVal[0]);
-        System.out.println("minPath:"+Arrays.toString(solution));
-    }
 
-    private void bfRec(ArrayList<Integer> visited, float[] currentMin){
-        if(visited.size()== lengths.length){
-            visited.add(0);
-            if(calcLength(visited)<currentMin[0]){
-                currentMin[0] = calcLength(visited);
-                solution = visited.stream().mapToInt(Integer::intValue).toArray();
-                return;
-            }
-        }
-        for (int i = 0; i < lengths.length; i++) {
-            if(!visited.contains(i)){
-                ArrayList<Integer> newVisited = (ArrayList<Integer>) visited.clone();
-                newVisited.add(i);
-                bfRec(newVisited, currentMin);
-            }
-        }
-    }
-
-    public float calcLength(ArrayList<Integer> l){
-        float ret = 0;
-        for (int i = 0; i < l.size()-1; i++) {
-            ret += lengths[l.get(i)][l.get(i+1)];
-        }
-        return ret;
-    }
 
 
 }
--- src/TSPSolver.java
+++ src/TSPSolver.java
@@ -1,2 +1,308 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class TSPSolver {
+
+    private final float[][] lengths;
+    ArrayList<Integer> solution = new ArrayList<>();
+
+    public TSPSolver(float[][] lengths) {
+        this.lengths = lengths;
+    }
+
+    public ArrayList<Integer> bruteForce(){
+        ArrayList<Integer> visited = new ArrayList<>();
+        visited.add(0);
+        float[] minVal = {Integer.MAX_VALUE};
+        bfRec(visited, minVal);
+        return solution;
+    }
+
+    private void bfRec(ArrayList<Integer> visited, float[] currentMin){
+        if(visited.size() == lengths.length){
+            visited.add(0);
+            if(calcLength(visited)<currentMin[0]){
+                currentMin[0] = calcLength(visited);
+                solution = visited;
+                return;
+            }
+        }
+        for (int i = 0; i < lengths.length; i++) {
+            if(!visited.contains(i)){
+                ArrayList<Integer> newVisited = new ArrayList<>(visited);
+                newVisited.add(i);
+                bfRec(newVisited, currentMin);
+            }
+        }
+    }
+
+
+
+    public ArrayList<Integer> tspHeldKarp() {
+        int n = lengths.length;
+        double[][] dp = new double[n][1 << n]; // Dynamic programming table
+        int[][] parent = new int[n][1 << n];   // To store parent cities
+        for (double[] row : dp) {
+            Arrays.fill(row, Double.POSITIVE_INFINITY);
+        }
+        dp[0][1] = 0; // Base case: Starting city with no other cities visited
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+            for (int u = 0; u < n; u++) {
+                if ((mask & (1 << u)) != 0) { // Check if city u is included in the subset represented by mask
+                    for (int v = 0; v < n; v++) {
+                        if ((mask & (1 << v)) != 0 && u != v) { // Check if city v is also included and is not the same as u
+                            double newDistance = dp[u][mask ^ (1 << v)] + lengths[u][v];
+                            if (newDistance < dp[v][mask]) {
+                                dp[v][mask] = newDistance;
+                                parent[v][mask] = u;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        double minDistance = Double.POSITIVE_INFINITY;
+        int minIdx = -1;
+        for (int v = 1; v < n; v++) {
+            double distance = dp[v][(1 << n) - 1] + lengths[v][0]; // Complete tour
+            if (distance < minDistance) {
+                minDistance = distance;
+                minIdx = v;
+            }
+        }
+
+        int[] tourIndices = new int[n+1];
+        int mask = (1 << n) - 1;
+        int idx = minIdx;
+        for (int i = n - 1; i >= 0; i--) {
+            tourIndices[i] = idx;
+            idx = parent[idx][mask];
+            mask ^= (1 << tourIndices[i]);
+        }
+
+        return Arrays.stream(tourIndices).boxed().collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public ArrayList<Integer> nn(){
+        ArrayList<Integer> ret = new ArrayList<>(lengths.length);
+        ret.add(0);
+        while(ret.size()<lengths.length){
+            int lastIdx = ret.get(ret.size()-1);
+            int minIdx = -1;
+            for (int i = 0; i < lengths.length; i++) {
+                if(ret.contains(i))continue;
+                if(minIdx == -1 || lengths[lastIdx][i]<lengths[lastIdx][minIdx]){
+                    minIdx = i;
+                }
+            }
+            ret.add(minIdx);
+        }
+
+        ret.add(0);
+        return ret;
+    }
+
+    public ArrayList<Integer> nn2(){
+        int l = lengths.length;
+        ArrayList<Integer> ret = new ArrayList<>(l+1);
+        ret.add(0);
+        ret.add(0);
+        for (int i = 1; i < l; i++) {
+            float minVal = Float.MAX_VALUE;
+            int minIdx = -1;
+            for (int j = 1; j <= i; j++) {
+                ret.add(j,i);
+                float val = calcLength(ret);
+                ret.remove(j);
+                if(val<minVal){
+                    minVal = val;
+                    minIdx = j;
+                }
+            }
+            ret.add(minIdx,i);
+        }
+
+        return ret;
+    }
+
+    public ArrayList<Integer> ant(){
+        int l = lengths.length;
+        float[][] weights = new float[l][l];
+
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < l; j++) {
+                weights[i][j] = i==j?0:1;
+            }
+        }
+
+
+        ArrayList<Integer> currentBestRoute = randomRoute();
+        float currentBestLen = calcLength(currentBestRoute);
+        for (double i = 1; i > 0; i -= 0.0001) {
+            ArrayList<Integer> currentRoute = new ArrayList<>(l+1);
+            currentRoute.add(0);
+            for (int j = 0; j < l-1; j++) {
+                ArrayList<Integer> candidates = new ArrayList<>(l);
+                for (int k = 0; k < l; k++) {
+                    if(!currentRoute.contains(k))candidates.add(k);
+                }
+                float rowSum = 0;
+                for (int c: candidates) {
+                    rowSum += (float) (weights[currentRoute.get(currentRoute.size()-1)][c] / (lengths[currentRoute.get(currentRoute.size()-1)][c]*1.5));
+                }
+                double rand = Math.random()*rowSum;
+                int newPoint = -1;
+                for (int c : candidates) {
+                    rand -= weights[currentRoute.get(currentRoute.size() - 1)][c] / (lengths[currentRoute.get(currentRoute.size()-1)][c]*1.5);
+                    if (rand <= 0.0f) {
+                        newPoint = c;
+                        break;
+                    }
+                }
+                currentRoute.add(newPoint);
+            }
+
+            currentRoute.add(0);
+
+            float thisLen = calcLength(currentRoute);
+            for (int j = 0; j < l-1; j++) {
+                weights[currentRoute.get(j)][currentRoute.get(j+1)] += 1/thisLen*100;
+            }
+
+            if(thisLen<currentBestLen){
+                currentBestLen = thisLen;
+                currentBestRoute = new ArrayList<>(currentRoute);
+            }
+
+
+        }
+
+        return currentBestRoute;
+    }
+
+
+    public ArrayList<Integer> randomRoute(){
+        int l = lengths.length;
+        ArrayList<Integer> route = new ArrayList<>(l+1);
+        route.add(0);
+        for (int i = 0; i < l-1; i++) {
+            ArrayList<Integer> candidates = new ArrayList<>(l);
+            for (int j = 0; j < l; j++) {
+               if(!route.contains(j))candidates.add(j);
+            }
+            route.add(candidates.get((int) (Math.random()*candidates.size())));
+        }
+        route.add(0);
+        return route;
+    }
+
+
+
+    public float calcLength(ArrayList<Integer> l){
+        float ret = 0;
+        for (int i = 0; i < l.size()-1; i++) {
+            ret += lengths[l.get(i)][l.get(i+1)];
+        }
+        return ret;
+    }
+
+
+    
+    public ArrayList<Integer> opt(ArrayList<Integer> list, int z){
+        ArrayDeque<ArrayList<Integer>> permQ = new ArrayDeque<>((int) Math.pow(list.size(),z));
+        int l = list.size();
+
+        for (int i = 1; i < l-1; i++) {
+            ArrayList<Integer> a = new ArrayList<>();
+            a.add(i);
+            permQ.add(a);
+        }
+
+        while(permQ.peek() != null && permQ.peek().size() < z){
+            ArrayList<Integer> a = permQ.pop();
+            for (int i = 1; i < l-1; i++) {
+                ArrayList<Integer> b = new ArrayList<>(a);
+                b.add(i);
+                permQ.add(b);
+            }
+        }
+
+        ArrayList<Integer> minRoute = new ArrayList<>(list);
+        float minLen = calcLength(minRoute);
+
+        while(!permQ.isEmpty()){
+            ArrayList<Integer> a = permQ.pop();
+            ArrayList<Integer> thisRoute = new ArrayList<>(list);
+
+            int temp = thisRoute.get(a.get(0));
+            for (int i = 0; i < a.size()-1; i++) {
+                thisRoute.set(a.get(i),thisRoute.get(a.get(i+1)));
+            }
+            thisRoute.set(a.get(a.size()-1),temp);
+
+            float thisLen = calcLength(thisRoute);
+
+            if(thisLen<minLen){
+                minRoute = thisRoute;
+                minLen = thisLen;
+            }
+
+
+
+        }
+
+        return minRoute;
+    }
+
+    public int fak(int n){
+        return n==1 ? 1:n*fak(n-1);
+    }
+
+    public void changePos(ArrayList<Integer> list, int a, int b){
+        int temp = list.get(b);
+        list.set(b,list.get(a));
+        list.set(a,temp);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
